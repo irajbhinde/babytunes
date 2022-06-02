67fad5e3b4ec6cc3e22eee429f67b3d2e42f6239
Raj Bhinde <68704186+irajbhinde@users.noreply.github.com>
2022-06-03 01:10:58 +0530

    feat : Nav bar

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,15 @@
+import "./nav.css"
+import "../../Utils/styles.css"
+
+export default function Nav(){
+    return (
+        <nav class="nav-bar">
+        <p>Baby Tunes</p>
+        <img
+          class="avatar"
+          src="https://endlessui.netlify.app/Images/avatar3.png"
+          alt="avatar1"
+        />
+      </nav>
+    )
+}
