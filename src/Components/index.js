55824f4d68a12c46b6f7c7ca34aaaba547268557
Raj Bhinde <68704186+irajbhinde@users.noreply.github.com>
2022-06-03 01:11:33 +0530

    feat : sidebar

---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+import Sidebar from "../Components/Sidebar/Sidebar"
+import { MustWatchCards, CategoryCard } from "./Cards/Cards"
+import Nav from "../Components/Nav/Nav"
+
+export {Sidebar, MustWatchCards, CategoryCard, Nav}
