dc69ba4fb531b09ad6a2fa69931bbd5fe1d3dc10
Raj Bhinde <68704186+irajbhinde@users.noreply.github.com>
2022-06-19 19:00:26 +0530

    feat : chips

---
+++ src/Components/Chips/Chips.jsx
@@ -0,0 +1,12 @@
+import "./chips.css"
+
+export default function Chips(){
+    return(
+       <section className="homepage-chips">
+        <div className="chips">Shows</div>
+        <div className="chips">Music</div>
+        <div className="chips">Learning</div>
+        <div className="chips">Explore</div>
+      </section> 
+    )
+}
