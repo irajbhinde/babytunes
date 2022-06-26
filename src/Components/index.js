6df53ad99f8f83ff68f3bfcf084871c58a448e71
Raj Bhinde <68704186+irajbhinde@users.noreply.github.com>
2022-06-26 19:11:16 +0530

    fix : deploy preview fix

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,7 +2,6 @@ import Sidebar from "../Components/Sidebar/Sidebar"
 import { MustWatchCards, CategoryCard, VideoListingCard } from "./Cards/Cards"
 import Nav from "../Components/Nav/Nav"
 import Chips from "../Components/Chips/Chips"
-import LoginCard from "./login_signup/LoginCard"
 import {PlayListModal} from "../Components/Playlist Modal/Modal"
 
-export {Sidebar, MustWatchCards, CategoryCard, Nav, Chips, VideoListingCard, LoginCard, PlayListModal}
+export {Sidebar, MustWatchCards, CategoryCard, Nav, Chips, VideoListingCard, PlayListModal}
