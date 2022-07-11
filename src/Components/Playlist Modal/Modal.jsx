c16dcc897b55b77209fb07e39ddf487a0277bc4a
Raj Bhinde <68704186+irajbhinde@users.noreply.github.com>
2022-07-12 00:56:38 +0530

    feat : Create new playlist at playlist page

--- src/Components/Playlist Modal/Modal.jsx
+++ src/Components/Playlist Modal/Modal.jsx
@@ -9,7 +9,7 @@ import { useState } from "react";
 import { useAuth, useVideo, useFeatures } from "../context/index";
 import { useNavigate } from "react-router-dom";
 
-export const PlayListModal = ({ video }) => {
+export const PlayListModal = () => {
   const { modal, setModal } = useVideo();
   const { auth } = useAuth();
   const navigate = useNavigate();
@@ -36,30 +36,30 @@ export const PlayListModal = ({ video }) => {
       <div className="listOfPlaylist">
         {playlist.length === 0 ? (
           <div>
-            <p>Kuch Nahi hai yaha</p>
+            <p>No Playlists Created</p>
           </div>
         ) : (
           <div className="listOfPlaylist-container flex_c">
-            {playlist.map((playlist) => {
+            {playlist.map((item) => {
               return (
                 <>
                   <div className="modal-content flex_r">
                     <input
                       className="playlist-checkbox"
-                      checked={videoPresentInPlaylist(playlist, currentVideo)}
+                      checked={videoPresentInPlaylist(item, currentVideo)}
                       onChange={(e) => {
                         if (e.target.checked) {
                           addVideoToPlaylist(
-                            playlist.title,
-                            playlist._id,
+                            item.title,
+                            item._id,
                             currentVideo,
                             featureDispatch,
                             authToken
                           );
                         } else {
                           deleteVideoFromPlaylist(
-                            playlist.title,
-                            playlist._id,
+                            item.title,
+                            item._id,
                             currentVideo,
                             featureDispatch,
                             authToken
@@ -68,13 +68,53 @@ export const PlayListModal = ({ video }) => {
                       }}
                       type="checkbox"
                     />
-                    <p>{playlist.title}</p>
+                    <p>{item.title}</p>
                   </div>
                 </>
               );
             })}
           </div>
         )}
+      </div>
+      <div className="modal-footer">
+        <input
+          type="text"
+          placeholder="Create a New Playlist"
+          onChange={(e) => {
+            setPlaylistName(e.target.value);
+          }}
+        />
+        <button className="plus-icon">
+          <i
+            onClick={() => {
+              authStatus
+                ? addToPlaylist(playlistName, featureDispatch, authToken)
+                : navigate("/login");
+            }}
+            className="fa-solid fa-plus fa-lg"
+          ></i>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export const CreateNewPlaylistModal = () => {
+  const { modal, setModal } = useVideo();
+  const { auth } = useAuth();
+  const navigate = useNavigate();
+  const { authToken, authStatus } = auth;
+  const { featureState, featureDispatch } = useFeatures();
+  const { playlist, currentVideo } = featureState;
+  const [playlistName, setPlaylistName] = useState("");
+  return (
+    <div className="playlist-modal">
+      <div className="header flex_r">
+        <p>Create a new Playlist</p>
+        <i
+          onClick={() => setModal(false)}
+          className="fa-solid fa-xmark fa-lg"
+        ></i>
       </div>
       <div className="modal-footer">
         <input
--- src/pages/Playlist Page/Playlist-Page.jsx
+++ src/pages/Playlist Page/Playlist-Page.jsx
@@ -1,32 +1,72 @@
 import "../Playlist Page/playlist-page.css";
 import "../../Utils/styles.css";
-import { Nav, PlaylistCard, Sidebar } from "../../Components";
-import { useFeatures } from "../../Components/context";
+import {
+  Nav,
+  PlaylistCard,
+  PlayListModal,
+  Sidebar,
+  CreateNewPlaylistModal,
+} from "../../Components";
+import { useFeatures, useVideo } from "../../Components/context";
 import { useNavigate } from "react-router-dom";
 
 export default function PlaylistPage() {
   const { featureState, featureDispatch } = useFeatures();
   const { playlist } = featureState;
   const navigate = useNavigate();
+  const { modal, setModal } = useVideo();
+
   return (
     <div className="playlistPage-wrapper">
       <Nav />
       <Sidebar />
       <div className="playlist-container">
         {playlist.length === 0 ? (
-          <div className="center-container flex_c">
-            <div className="empty-list-msg">Your Playlist is Empty</div>
-            <button
-              onClick={() => {
-                navigate("/");
-              }}
-              className="btn-explore"
-            >
-              Explore
-            </button>
-          </div>
+          <>
+            {modal && (
+              <div className="playlist-overlay">
+                <CreateNewPlaylistModal />
+              </div>
+            )}
+            <div className="create-playlist flex_r">
+              <button
+                onClick={() => {
+                  setModal(true);
+                }}
+                className="btn-createPlaylist"
+              >
+                Create Playlist
+              </button>
+            </div>
+            <div className="center-container flex_c">
+              <div className="empty-list-msg">Your Playlist is Empty</div>
+              <button
+                onClick={() => {
+                  navigate("/");
+                }}
+                className="btn-explore"
+              >
+                Explore
+              </button>
+            </div>
+          </>
         ) : (
           <div>
+            {modal && (
+              <div className="playlist-overlay">
+                <CreateNewPlaylistModal />
+              </div>
+            )}
+            <div className="create-playlist flex_r">
+              <button
+                onClick={() => {
+                  setModal(true);
+                }}
+                className="btn-createPlaylist"
+              >
+                Create Playlist
+              </button>
+            </div>
             <div className="playlist">
               <PlaylistCard key={playlist._id} />
             </div>
